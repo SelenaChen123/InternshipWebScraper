2d5a3a66a892eb19b969a87e72d53fc1efd0f8da
Selena <selenachen2000@gmail.com>
2021-12-05 23:26:15 -0500

    Completed

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # InternshipWebScraper
 
+## Project Description:
+
 For the final project, you will:
 
 -   Develop a documented dataset to support looking at a particular problem
@@ -7,3 +9,28 @@ For the final project, you will:
 -   Have reproducible code for retrieval from the web, data cleaning, and final a dataset
 -   Discuss the dataset, the process above, and demonstrate how it addresses the original problem statement in a separate 1-2 page summary
 -   Submit your report to Moodle as one file
+
+## Project Documentation
+
+### Problem Statement:
+
+As an upperclassman Computer Science major stressing over having to find internships to gain experience and give us a leg up when it comes to our job search after graduation, finding internships to apply for can be a tricky task. It’s important to weigh all of the factors that are important to us when determining which internships to apply for. One such major factor is compensation. As greedy as that sounds, the sad truth is that most college students have hefty tuition loans, expensive rent, and utility bills to pay for, so internships with higher pay are much more appealing. There are so many companies offering software engineering internships, but where should students start looking for the ones that pay the most?
+
+### Description
+
+The purpose of this web scraper is to scrape the levels.fyi website, which lets you compare salary information at different career levels for positions at various companies, in order to consolidate a fairly comprehensive list of software engineering internships. This dataset only shows information about the software engineering internships that are currently open for applications. For the ones that are still open for applications, the dataset shows which ones pay the most, if the companies provide housing accommodations, and the link to apply for the internship directly on the company’s website. With this information, students looking for software engineering internships have a great resource that lists higher-paying internships that they can still apply for.
+
+### Metadata
+
+#### Ethics
+
+There are no public APIs that I could find for this information, but the information from this website is open to the public. Selenium, a browser automation tool, is used to establish a connection to the website, but since I am only scraping information from a single webpage rather than several webpages, there shouldn’t be an issue of creating repetitive connections that would potentially damage the website’s stability and start a DDoS attack. I also make sure to cite the source and give credit to the co-founders of the website so that I am respectful of the content creators’ work.
+
+#### Sources:
+
+https://www.levels.fyi/
+
+#### Attribution:
+
+Zaheer Mohiuddin and Zuhayeer Musa
+
--- main.py
+++ main.py
@@ -1,22 +1,144 @@
 # Imports libraries
-import requests as req
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup as bs
+import pandas as pd
 
-# Sets base URL
-baseURL = "https://www.levels.fyi/internships/"
+# Sets up constants
+BASE_URL = "https://www.levels.fyi/internships/"
+DOUBLE_QUOTES = "\""
 
-
+# Creates driver and gets the webpage
 def getPage():
-    # Gets the page and parses it
-    html = req.get(baseURL + str(id)).text
-    page = bs(html, "html.parser")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome('chromedriver', options=chrome_options)
+    driver.get(BASE_URL)
 
-    return page
+    try:
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located(
+            (By.CLASS_NAME, "internships-table")))
 
+        html = driver.page_source
+        page = bs(html, "html.parser")
+    finally:
+        driver.quit()
 
+        return page
+
+# Finds the table of information and extracts all the relevant data to a list
+def extractToList(page):
+    table = page.find("table", {"class": "internships-table"})
+
+    tr = table.tbody.find_all("tr")
+    rows = []
+    for row in tr:
+        if row.has_attr("data-index"):
+            row_data = row.find_all("td")
+            is_open = row_data[3].find("a").text.strip() == "Apply"
+            if is_open:
+                rows.append(row)
+
+    data = []
+    for row in rows:
+        row_data = row.find_all("td")
+
+        company = row_data[0].find("h6").text.strip()
+
+        hourly_salary = ""
+        has_hourly_salary_info = row_data[1].find("h6") is not None
+        if has_hourly_salary_info:
+            hourly_salary = row_data[1].find("h6").text.strip()
+
+        housing = ""
+        has_housing_info = row_data[2].find("p") is not None
+        if has_housing_info:
+            housing = row_data[2].find("p").text.strip()
+
+        apply_link = row_data[3].find("a")["href"]
+
+        item = [company, hourly_salary, housing, apply_link]
+        data.append(item)
+
+    return data
+
+# Finds the table of information and extracts all the relevant data to a dictionary
+def extractToDict(page):
+    table = page.find("table", {"class": "internships-table"})
+
+    tr = table.tbody.find_all("tr")
+    rows = []
+    for row in tr:
+        if row.has_attr("data-index"):
+            row_data = row.find_all("td")
+            is_open = row_data[3].find("a").text.strip() == "Apply"
+            if is_open:
+                rows.append(row)
+
+    dict = []
+    for row in rows:
+        row_data = row.find_all("td")
+
+        company = row_data[0].find("h6").text.strip()
+
+        hourly_salary = ""
+        has_hourly_salary_info = row_data[1].find("h6") is not None
+        if has_hourly_salary_info:
+            hourly_salary = row_data[1].find("h6").text.strip()
+
+        housing = ""
+        has_housing_info = row_data[2].find("p") is not None
+        if has_housing_info:
+            housing = row_data[2].find("p").text.strip()
+
+        apply_link = row_data[3].find("a")["href"]
+
+        item = {"Company": company, "Hourly Salary": hourly_salary,
+                "Housing": housing, "Apply Link": apply_link}
+        dict.append(item)
+
+    return dict
+
+# Creates a pandas dataframe out of the data
+def createTable(data):
+    dataframe = pd.DataFrame(data)
+    dataframe.columns = ["Company", "Hourly Salary", "Housing", "Apply Link"]
+    dataframe.sources = [BASE_URL]
+    dataframe.attribution = ["Zaheer Mohiuddin", "Zuhayeer Musa"]
+
+    return dataframe
+
+# Prints the dictionary of data to a CSV
+def printToCSV(dict):
+    with open("internships.csv", "w") as out:
+        is_header = True
+        for entry in dict:
+            if is_header:
+                is_header = False
+                for key in dict[0].keys():
+                    out.write(f"{DOUBLE_QUOTES}{key}{DOUBLE_QUOTES}")
+                    if key != "Apply Link":
+                        out.write(", ")
+                out.write("\n")
+
+            for key in dict[0].keys():
+                out.write(f"{DOUBLE_QUOTES}{entry[key]}{DOUBLE_QUOTES}")
+                if key != "Apply Link":
+                    out.write(", ")
+            out.write("\n")
+
+# Starting point for the program
 def main():
     page = getPage()
-    print(page.find("table", {"class": "internships-table"}))
+    dict = extractToDict(page)
+    printToCSV(dict)
+
+    data = extractToList(page)
+    createTable(data)
 
 
 print()
